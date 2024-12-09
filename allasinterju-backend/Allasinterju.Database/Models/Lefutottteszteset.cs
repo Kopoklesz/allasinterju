07d3af3cf385484dfb5ededce36caf2d617ffe41
WySoEdgy <sabjanoliver726@gmail.com>
2024-12-09 21:29:49 +0100

    merge

--- allasinterju-backend/Allasinterju.Database/Models/Lefutottteszteset.cs
+++ allasinterju-backend/Allasinterju.Database/Models/Lefutottteszteset.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 
 namespace Allasinterju.Database.Models;
@@ -11,7 +11,6 @@ public partial class Lefutottteszteset
 
     public int Kitoltottkerdesid { get; set; }
 
-<<<<<<< HEAD
     public string? Kimenet { get; set; }
 
     public bool? Helyes { get; set; }
@@ -21,11 +20,6 @@ public partial class Lefutottteszteset
     public string? Hibakimenet { get; set; }
 
     public double? Futasido { get; set; }
-=======
-    public int? Kimenet { get; set; }
-
-    public int Helyes { get; set; }
->>>>>>> 2daedc2fac9c9c8eb3c1c10b32464ff7fa763403
 
     public virtual Kitoltottkerde Kitoltottkerdes { get; set; } = null!;
 
