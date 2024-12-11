6390beaad0fbe6dc6f2946b7d11f66798e4bd559
nemadtakibence <kovacs.bence.8@outlook.com>
2024-12-11 01:42:07 +0100

    Competence fix

--- allasinterju-backend/Allasinterju.Core/DTOs/DtoCompetence.cs
+++ allasinterju-backend/Allasinterju.Core/DTOs/DtoCompetence.cs
@@ -3,7 +3,27 @@ using Allasinterju.Database.Models;
 public class RDtoCompetence{
     public int Id{get;set;}
     public string Type{get;set;}
+    public string Level{get;set;}
     public RDtoCompetence(Kompetencium k){
+        Id=k.Id;
+        Type=k.Tipus;
+    }
+    public RDtoCompetence(Allaskompetencium ak){
+        Id=ak.Kompetenciaid;
+        Type=ak.Kompetencia.Tipus;
+        Level=ak.Szint;
+    }
+    public RDtoCompetence(Felhasznalokompetencium fk){
+        Id=fk.Kompetenciaid;
+        Type=fk.Kompetencia.Tipus;
+        Level=fk.Szint;
+    }
+}
+
+public class RDtoCompetenceOnly{
+    public int Id{get;set;}
+    public string Type{get;set;}
+    public RDtoCompetenceOnly(Kompetencium k){
         Id=k.Id;
         Type=k.Tipus;
     }
