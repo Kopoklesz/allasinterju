3539a04ba7483639b91acbd5466c85677da20d7b
WySoEdgy <sabjanoliver726@gmail.com>
2024-12-11 01:22:57 +0100

    dsada

--- allasinterju-backend/Allasinterju.Database/Models/AllasinterjuContext.cs
+++ allasinterju-backend/Allasinterju.Database/Models/AllasinterjuContext.cs
@@ -119,7 +119,7 @@ public partial class AllasinterjuContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-PV9UB7M;Initial Catalog=allasinterju;Encrypt=False;TrustServerCertificate=True;Integrated Security=True;");
+        => optionsBuilder.UseSqlServer("Data Source=DESKTOP-8DSO4K8;Initial Catalog=allasinterju;Encrypt=False;TrustServerCertificate=True;Integrated Security=True;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
